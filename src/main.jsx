58880458c0258e7a61f0cf09a57dd03d1cb439f4
Armen Sepanyan <armensepanyan2000@gmail.com>
2024-04-27 00:39:22 -0700

    hello

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Hello from './Hello/Hello'
+const jsxElements = (
+  <Hello/>
+)
+ReactDOM.createRoot(document.getElementById('root')).render(jsxElements)
